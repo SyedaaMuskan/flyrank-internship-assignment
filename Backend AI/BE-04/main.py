3891e1b62690ff5d336bd9682870316c89a4efce
muskan <msyeda415@gmail.com>
2026-08-27 12:30:28 +0500

    stage 1

--- Backend AI/BE-04/main.py
+++ Backend AI/BE-04/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI,HTTPException
 import os 
 from supabase import create_client, Client
 from dotenv import load_dotenv
@@ -14,4 +14,43 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
+
+@app.post("/auth/signup",status_code=201)
+def signup(data:dict):
+    email= data.get("email")
+    password= data.get("password")
+    if not email or not password:
+        raise HTTPException(status_code=400, detail="Email and password are required")
+    response = supabase.auth.signup({"email": email, "password": password})
+    if response.user is None:
+        raise HTTPException(status_code=400, detail=response.error.message)
+    return {"user": response.user.model_dump()}   
+
+@app.post("/auth/login")
+def login(data: dict):
+    email = data.get("email")
+    password = data.get("password")
+
+    if not email or not password:
+        raise HTTPException(
+            status_code=400,
+            detail="Email and password are required"
+        )
+
+    response = supabase.auth.sign_in_with_password({
+        "email": email,
+        "password": password
+    })
+
+    if response.session is None:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid login credentials"
+        )
+
+    return {
+        "access_token": response.session.access_token,
+        "refresh_token": response.session.refresh_token
+    }    
+
     
